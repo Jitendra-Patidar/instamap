7496ce3ae6b97fc3ce60cd06914f29ffa378d251
Richard VanBreemen <rvbsanjose@me.com>
2012-12-31 13:33:53 -0800

    Added page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def comma_number(number)
     number_with_delimiter(number, :delimiter => ",")
   end
+
+  def title(page_title)
+  	content_for(:title) { page_title }
+  end
 end
