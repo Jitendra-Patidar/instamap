eb3f5a9216d75f343ed89e8a178ee342f47c12a3
Richard VanBreemen <rvbsanjose@me.com>
2013-01-04 16:59:14 -0800

    Added more RSpec tests

--- spec/controllers/instamap_controller_spec.rb
+++ spec/controllers/instamap_controller_spec.rb
@@ -3,30 +3,78 @@ require "spec_helper"
 describe InstamapController do
 
 	describe "#new" do
-		it
+		it "should load the page" do
+			get :index
+			response.should be_success
+		end
+
+		it "should render the index view" do
+			get :index
+			response.should render_template(:index)
+		end
 	end
 
 	describe "#places" do
 		context "when arguments are not passed correctly" do
 		  
-		  it "should expect two arguments" do
+		  it "should expect at least two arguments" do
 		  	expect { Instagram.media_search(40.7142) }.to raise_error
 			end
 
-			it "should have a length of 0" do
+			it "should have an array length of 0" do
 				media = Instagram.media_search(-74.0064, -40.7142)
 				media.data.length.should eq 0
 			end
 		end
 
-		context "when passed the correct arguments" do
+		context "when arguments are passed correctly" do
 
 			it "should not raise error" do
-				expect { Instagram.media_search(40.7142, 74.0064) }.to_not raise_error
+				expect { Instagram.media_search(40.7142, -74.0064) }.to_not raise_error
 			end
 
 			it "should not return more images then count passed in the options hash" do
 				Instagram.media_search(40.7142, -74.0064, options = { count: 5 }).length.should_not be > 5
+			end
+		end
+	end
+
+	describe "#comments" do
+		context "when passed the correct media id" do
+	  
+		  it "should get comments for the media" do
+		    get :comments, { id: "361125177477624988_14895060" }
+		    response.should be_success
+		  end
+
+		  it "should not raise an error" do
+		  	expect { get :comments, { id: "361125177477624988_14895060" } }.to_not raise_error
+		  end
+
+		  it "should return an array of length greater then 0 for media with comments" do
+		  	get :comments, { id: "361125177477624988_14895060" }
+		  	response.body.length.should be > 0
+		  end
+
+		  it "should return status 200" do
+		  	get :comments, { id: "361125177477624988_14895060" }
+		  	response.status.should be 200
+		  end
+		end
+
+		context "when passed the incorrect media id" do
+
+			it "should raise an error" do
+				expect { get :comments, { id: "3611251774" } }.to raise_error
+			end
+
+			it "should return an empty array for media with comments" do
+				get :comments, { id: "223402934" }
+				response.body.should eq("[]")
+			end
+
+			it "should return Instagram::BadRequest" do
+				expect { get :comments, { id: "lskefj" } }.to raise_error(Instagram::BadRequest)
 			end
 		end
 	end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,1 +1,79 @@
 require "spec_helper"
+
+describe UsersController do
+
+	describe "#show" do
+		
+		before :each do
+			@user = User.find_by_username("rvbsanjose")
+		end
+
+		context "when user is an instamap user" do
+		  
+		  it "should have a user with status 200" do
+		    get :show, { username: "rvbsanjose" }
+		    response.status.should be 200
+		  end
+
+		  it "should contain the user" do
+		  	get :show, { username: "rvbsanjose" }
+		    response.body.should_not be_nil
+		  end
+
+		  it "should get the users media" do
+		  	images = Instagram.user_recent_media(@user.instagram_id, options = { access_token: @user.access_token })
+		  	images.should_not be_nil
+		  end
+
+		  it "should get the users stats" do
+		  	stats = Instagram.user(@user.instagram_id)
+		  	stats.should_not be_nil
+		  end
+
+		  it "should get who the user follows" do
+		  	follows = Instagram.user_follows(@user.instagram_id, options = { access_token: @user.access_token })
+		  	follows.should_not be_nil
+		  end
+
+		  it "should get who follows the user" do
+		  	following = Instagram.user_followed_by(@user.instagram_id, options = { access_token: @user.access_token })
+		  	following.should_not be_nil
+		  end
+		end
+
+		context "when user is an instagram user" do
+
+			it "should be able to grab the users media" do
+				@instagram_user = Instagram.user_search("stancenation").first
+				expect { Instagram.user_recent_media(@instagram_user.id, options = { access_token: @user.access_token }) }.to_not raise_error
+			end
+
+			it "should be able to grab who the user follows" do
+				@instagram_user = Instagram.user_search("stancenation").first
+				expect { Instagram.user_follows(@instagram_user.id, options = { access_token: @user.access_token }) }.to_not raise_error
+			end
+
+			it "should be able to grab who follows the user" do
+				@instagram_user = Instagram.user_search("stancenation").first
+				expect { Instagram.user_followed_by(@instagram_user.id, options = { access_token: @user.access_token }) }.to_not raise_error
+			end
+		end
+
+		context "when it does not have a user param" do
+
+			it "should return status 302" do
+				get :show, { username: "" }
+				response.status.should be 302
+			end
+		end
+	end
+  
+  describe "#logout" do
+	
+  	it "should clear the sesssion" do
+  		session[:user] = "Larry"
+  		get :logout
+  		session[:user].should be_nil
+  	end
+	end
+end
